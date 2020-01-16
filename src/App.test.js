ca408c3ac998781a4dbf921335b65047c49b54f2
Gdraven99 <DavidJYopp@gmail.com>
2020-01-15 20:29:06 -0500

    added a finByTestAttr Function for refactor tests are in green phase still

--- src/App.test.js
+++ src/App.test.js
@@ -17,19 +17,30 @@ const setup = (props={}, state=null) => {
   return shallow(<App {...props}/>)
 }
 
+/**
+* Return ShallowWrapper containing node(s) with the  given data-test value.
+* @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+* @param {string} val - Value od data-test attribute for search
+* @returns {ShallowWrapper}
+*/
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
 test('renders without error', () => {
   const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const appComponent = findByTestAttr(wrapper, 'component-app')
   expect(appComponent.length).toBe(1);
 });
 test('renders increment button', () => {
   const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']")
+  const button = findByTestAttr(wrapper, 'increment-button')
   expect(button.length).toBe(1);
 });
 test('renders counter display', () => {
   const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
   expect(counterDisplay.length).toBe(1);
 });
 test('counter starts at 0', () => {
